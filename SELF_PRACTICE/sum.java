1829fee6fb2dec39f52e352c54319404810f0f1a
madhavsameer <madhavsameer97@gmail.com>
2024-05-15 11:45:09 +0530

    Day 1 traning pushed

---
+++ SELF_PRACTICE/sum.java
@@ -0,0 +1,11 @@
+
+public class sum {
+
+    public static void main(String[] args) {
+        int a=6;
+        int b=7;
+        int c=a+b;
+        System.out.println("The sum of these numbers is "+c);
+    }
+    
+}
